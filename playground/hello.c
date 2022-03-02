8542076b60dbe78d49837ec312ab41324ee8c215
xjd <xjd@cryptape.com>
2022-03-02 17:45:37 +0800

    Update

--- playground/hello.c
+++ playground/hello.c
@@ -4,18 +4,28 @@
 int main() {
   printf("entering main() ...\n");
 
-  uint64_t avl = 100;
+  uint64_t v1_values[4] = {0};
+  uint64_t v2_values[4] = {1, 1, 1, 1};
+  uint64_t v3_values[4] = {2, 2, 2, 2};
+
+  uint64_t res_values[4] = {0};
+  uint64_t avl = 4;
   uint64_t vl = 0;
-  // can't support when lmul = 2, 4, 8
-  asm volatile("vsetvli t0, %1, e32, m1\n"
-				"mv %0, t0"
-               : "=r" (vl)
+  asm volatile("vsetvli t0, %1, e64, m2\n"
+               "mv %0, t0"
+               : "=r"(vl)
                : "r"(avl)
                : /* no registers */
   );
+
+  asm volatile("vle64.v v2, (%0)" : : "r"(v1_values) :);
+  asm volatile("vle64.v v10, (%0)" : : "r"(v2_values) :);
+  asm volatile("vle64.v v20, (%0)" : : "r"(v3_values) :);
+  asm volatile("vadd.vv v2, v10, v20");
+  asm volatile("vse64.v v2, (%0)" : : "r"(res_values) :);
+
   printf("avl = %d, vl = %d\n", avl, vl);
-  asm volatile("vadd.vv v21, v1, v11");
-  int i = 100;
+  printf("result0 = %lld, result1 = %lld\n", res_values[0], res_values[1]);
   printf("leaving main()\n");
   return 0;
 }
---
+++ playground/vadd_vv.s
@@ -0,0 +1,3 @@
+
+vsetivli t0, 8, e64, m2
+vadd.vv v2, v10, v20
